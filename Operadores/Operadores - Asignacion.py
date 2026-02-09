f7076836f9847f13ddea20666806a3c2ea9aa124
Johndev <arocajhon12@gmail.com>
2026-02-09 05:47:38 -0500

    Docs; Se añade la carpeta oepradores y el nuevo operador WALRUS

---
+++ Operadores/Operadores - Asignacion.py
@@ -0,0 +1,34 @@
+#Operadores Asignacion
+
+x = 5
+
+# x = x + 3 (opcion tradicional)
+x += 3  #(Opcion con operador de asignacion) Suma
+print("El valor de x es:", x)
+
+x -= 3 #Resta
+print("El valor de x es:", x)
+
+x *= 3 #Multiplicacion
+print("El valor de x es:", x)
+
+x /= 3 #Division
+print("El valor de x es:", x)
+
+x %= 2 #Modulo (Resto)
+print("El valor de x es:", x)
+
+y = 20
+
+y //= 2 #Division entera
+print("El valor de y es:", y)
+
+y **= 3 #Exponente 10 * 10 * 10
+print("El valo de y es;", y)
+
+
+#WALRUS (Asignacion con operador logico "Morsa")
+print(z := 3) #Nos va a dejar asignar el valor dentro de un metodo una variable
+print(z)
+
+
---
+++ Operadores/Operadores Aritmeticos.py
@@ -0,0 +1,45 @@
+# Operadores aritmeticos
+
+x = 5
+y = 10
+
+#suma +
+suma = x + y
+print("La suma es:", suma)
+
+#Resta -
+resta = x - y
+print("La resta es:", resta)
+
+#Multiplicación *
+multiplicacion = x * y 
+print("La multiplcación es:", multiplicacion)
+
+#Division / (Retorna un Float)
+division = y / x
+print("La division es:", division)
+
+#Modulo % (Retorna el residuo de la división)
+modulo = y % x
+print("El modulo es:", modulo)
+
+#Potencia **
+potencia = y ** x
+print("La potencia es:", potencia)
+
+#Division entera // (Retorna un entero redondeado hacia abajo)
+division_entera = y // x
+print("La division entera es:", division_entera)
+
+#Ejemplo de uso del operador modulo para determinar si un numero es par o impar
+par = 7
+print("El numero es par:", par % 2 == 0)
+
+# Predencia de operadores
+
+# Parentesis
+# Exponentes
+# Multiplicaciones, Divisiones, Divisiones enteras, restos
+# Sumas y restas
+# Comparaciones de identidad y pertenencia
+# Logicos
