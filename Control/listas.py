7c00897f3e1d19b1f053b70b32736864b3d82814
Johndev <arocajhon12@gmail.com>
2026-02-16 22:47:15 -0500

    "Se agrega la funcion de Lista en la carpeta de control"

---
+++ Control/listas.py
@@ -0,0 +1,14 @@
+#Listas: Las listas son ordenadas, modificables y permiten valores duplicados
+
+# indices :   0         1           2        3
+frutas = ["Manzana", "Naranja", "Mango", "Naranja"]
+print(frutas)
+print(type(frutas))
+
+frutas[1] = "Banana"
+
+print(frutas[1])
+print(frutas)
+
+listas=["John", 12,"Aroca"]
+print(listas)
