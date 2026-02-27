2f55bf93b4149b5a42e60eb19cdba749bfbe4ce8
Johndev <arocajhon12@gmail.com>
2026-02-27 12:13:26 -0500

    Se crea la carpera modulos y crea ekl archivo main y el archivo operaciones. Se añaden ejemplo.

---
+++ modulos/main.py
@@ -0,0 +1,12 @@
+from operaciones import sumar, restar, multiplicar, dividir
+
+print(sumar(2,3))
+print(restar(2,3))
+print(multiplicar(2,3))
+print(dividir(10,5))
+
+
+"""
+Los modulos son archivos que contienen codigo python que puede ser importado y utilizado en otros archivos.
+permiten organizar el codigo en partes mas pequeñas y reutilizables, ademas de mejorar la legibilidad del mismo.
+"""
